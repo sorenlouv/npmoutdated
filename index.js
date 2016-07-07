5cb15a6ac794fc58cb2e5b29d8199efae58a7059
Søren Louv-Jansen <sorenlouv@gmail.com>
2016-07-07 19:21:44 +0200

    Added node env header

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var RegClient = require('npm-registry-client');
 var Promise = require('bluebird');
 var _ = require('lodash');
@@ -7,31 +9,33 @@ var fs = require('fs');
 var package = JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf8'));
 
 function getLatestVersion(name) {
-	return new Promise((resolve, reject) => {
-		client.get('https://registry.npmjs.org/' + name, {}, (error, data, raw, res) => {
-			if(error) {
-				return reject(error);
-			}
-			resolve(data['dist-tags'].latest);
-		});
-	});
+    return new Promise(function(resolve, reject) {
+    	var options = {};
+        client.get('https://registry.npmjs.org/' + name, options, function(error, data, raw, res) {
+            if (error) {
+                return reject(error);
+            }
+            resolve(data['dist-tags'].latest);
+        });
+    });
 }
 
 function getUpdatedDependencies(deps) {
-	var promises = _.map(deps, (currentVersion, name) => {
-		return getLatestVersion(name).then(latestVersion => [name, latestVersion]);
-	});
-
-	return Promise.all(promises).then(dependencies => sortObj(_.fromPairs(dependencies)));
+    var promises = _.map(deps, function(currentVersion, name) {
+        return getLatestVersion(name).then(function(latestVersion) {
+            return [name, latestVersion];
+        });
+    });
+    return Promise.all(promises).then(function(dependencies) {
+        return sortObj(_.fromPairs(dependencies));
+    });
 }
-
 Promise.all([
-	getUpdatedDependencies(package.dependencies),
-	getUpdatedDependencies(package.devDependencies)
-]).spread((dependencies, devDependencies) => {
-	package.dependencies = dependencies;
-	package.devDependencies = devDependencies;
-
-	console.log('Updated package.json:');
-	console.log(JSON.stringify(package, null, 4), '\n');
+    getUpdatedDependencies(package.dependencies),
+    getUpdatedDependencies(package.devDependencies)
+]).spread(function(dependencies, devDependencies) {
+    package.dependencies = dependencies;
+    package.devDependencies = devDependencies;
+    console.log('Updated package.json:');
+    console.log(JSON.stringify(package, null, 4), '\n');
 });
